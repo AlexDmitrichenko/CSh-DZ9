ac42b204e935afb5b665d86f5541d6e39b86d9ca
Alex <best1972@list.ru>
2023-01-30 17:59:37 +0300

    made part 1 task 2

--- Program.cs
+++ Program.cs
@@ -10,3 +10,22 @@ string PrintNumbers(int start, int end)
     if (start == end) return start.ToString();
     return (start + " " + PrintNumbers(start - 1, end));
 }*/
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+//в промежутке от M до N.   M = 1; N = 15 -> 120    M = 4; N = 8. -> 30
+Console.Write("Введите число M: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SunNumbers(M, N));
+// Method---------------------------------
+int SunNumbers(int M, int N)
+// {
+//     if(M == N) return M+N;
+//     if(M < N) return (N + M) * (N - M + 1)/2;
+//     return (M + N) * (M - N + 1)/2;
+// }
+{
+    if(M == N) return M+N;
+    if(M < N) return (M + SunNumbers(N, M));
+    return (N + SunNumbers(M, N));
+}
